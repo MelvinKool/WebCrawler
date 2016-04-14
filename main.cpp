6d80f909b965c660d3592a268963b138a771feac
Melvin <melvinsido@msn.com>
2016-04-14 16:11:21 +0200

    Error handling and method

--- main.cpp
+++ main.cpp
@@ -12,55 +12,32 @@ size_t writeCallback(char* buf, size_t size, size_t nmemb, void* up)
     return size*nmemb; //tell curl how many bytes we handled
 }
 
-// std::string getPage(std::string url){
-//         CURLcode code;
-//         CURL *curl;
-//         curl_global_init(CURL_GLOBAL_DEFAULT);
-//         std::string readBuffer;
-//         curl = curl_easy_init();
-//         if (curl == NULL)
-//                 throw std::runtime_error("Failed to create CURL connection");
-//         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-//         if (code != CURLE_OK)
-//                 throw std::runtime_error("There is something wrong with your URL");
-//         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
-//         curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); //tell curl to output its progress
-//         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-//         code = curl_easy_perform(curl);
-//         curl_easy_cleanup(curl);
-//         if (code != CURLE_OK)
-//                 throw std::runtime_error("Failed to retrieve web page");
-//         return readBuffer;
-// }
-
-int main(int argc, char* argv[] )
-{
+std::string getPage(std::string url){
         CURLcode code;
         CURL *curl;
         curl_global_init(CURL_GLOBAL_DEFAULT);
         std::string readBuffer;
         curl = curl_easy_init();
         if (curl == NULL)
-        {
-                fprintf(stderr, "Failed to create CURL connection\n");
-                exit(EXIT_FAILURE);
-        }
-        curl_easy_setopt(curl, CURLOPT_URL, "https://curl.haxx.se/libcurl/c/htmltidy.html");
-        if (code != CURLE_OK)
-        {
-                fprintf(stderr, "Something is wrong wit\n");
-                exit(EXIT_FAILURE);
-        }
+                throw std::runtime_error("Failed to create CURL connection");
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        // if (code != CURLE_OK)
+        //         throw std::runtime_error("There is something wrong with your URL");
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); //tell curl to output its progress
+        // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); //tell curl to output its progress
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
         code = curl_easy_perform(curl);
-        std::cout << readBuffer << std::endl;
         curl_easy_cleanup(curl);
         if (code != CURLE_OK)
-        {
-                exit(EXIT_FAILURE);
-        }
+                throw std::runtime_error("Failed to retrieve web page");
+        return readBuffer;
+}
+
+int main(int argc, char* argv[] )
+{
+        std::string url = "https://curl.haxx.se/libcurl/c/htmltidy.html";
+        std::string pageContent = getPage(url);
+        std::cout << pageContent << std::endl;
         //curl_global_cleanup(); niet thread safe, anders wel gebruiken
         return EXIT_SUCCESS;
 }
